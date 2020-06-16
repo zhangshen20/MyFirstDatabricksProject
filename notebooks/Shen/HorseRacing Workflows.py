c25d7f04c25d27243a2c41d6424ccd9e16121d74
Shen Zhang <zhangshen20@hotmail.com>
2020-06-16 21:23:35 +1000

    TEST

---
+++ notebooks/Shen/HorseRacing Workflows.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %run "./HorseRacingRunnerForm"
+
+# COMMAND ----------
+
+# MAGIC %run "./HorseRacingRunnerStarts"
+
+# COMMAND ----------
+
+# MAGIC %run "./HorseRacingRunnerPreviousStarts"
+
+# COMMAND ----------
+
+# MAGIC %run "./HorseRacingTrainerPreviousStarts"
+
+# COMMAND ----------
+
+# MAGIC %run "./HorseRacingRiderPreviousStarts"
+
+# COMMAND ----------
+
+# MAGIC %run "./HorseRacingRunnerWinningDistances"
